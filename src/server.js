3d0b56664eef38c1fa2fa71a7388d1a1b8bd799c
bitda <apple380g@gmail.com>
2023-03-21 00:10:42 +0900

    server setting

--- src/server.js
+++ src/server.js
@@ -1,4 +1,9 @@
 import express from "express";
 const app = express();
-app.listen(3000);
+app.set("view engine", "pug"); //setting view engine
+app.set("views", __dirname + "/views"); //let express know where is template
+app.use("/public", express.static(__dirname + "/public")); // createing public url to share user files
+const handleListen = () => console.log(`Listening on from 3000`);
+app.get("/", (req, res) => res.render("home")); //route handler that renders home.pug
+app.listen(3000, handleListen);
 console.log("hello");
